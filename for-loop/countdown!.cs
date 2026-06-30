d23002b3bc0b3b148eb81b533edac19274849b0f
naen2234 <ys2918988@gmail.com>
2026-06-30 10:30:01 +0300

    Create countdown!.cs

---
+++ for-loop/countdown!.cs
@@ -0,0 +1,18 @@
+using System;
+namespace MyFirstProgram
+{
+class Program
+    {
+        static void Main(string[] args)
+        {
+           /* for ( int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }*/
+            for (int i = 10; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
